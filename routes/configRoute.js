69cb3d290ed64fe479596f9d874fa8037ac9f50f
Shaul Kimel <shaullk600@gmail.com>
2025-08-10 11:17:28 +0300

    update: Interfacing with complaint submission details and returning an ID value

--- routes/configRoute.js
+++ routes/configRoute.js
@@ -3,9 +3,13 @@ import { initNewComplaint } from "../controllers/complaintControllers.js";
 
 export default function configRoutes(app) {
 
-   
-    app.post('/submit/', initNewComplaint);
 
-    
+    app.post('/submit/', (req, res, next) => {
+        console.log(req.body);
+        next();
+    }
+        , initNewComplaint);
+
+
 
 }
